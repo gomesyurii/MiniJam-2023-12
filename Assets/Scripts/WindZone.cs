2704fd943455bc5f6b0038da704e213d53daea9a
Yuri de Jesus Gomes <dev.yurigomes@gmail.com>
2023-12-24 13:54:55 -0300

    Scene 1 + anims

--- Assets/Scripts/WindZone.cs
+++ Assets/Scripts/WindZone.cs
@@ -7,11 +7,24 @@ public class WindZone : MonoBehaviour
 {
     [SerializeField] private float windForce = 100f;
     [SerializeField] private bool canWind = false;
+    [SerializeField] private SpriteRenderer buttonSprite;
 
+    void Start()
+    {
+        buttonSprite.color = Color.red;
+    }
 
     public void SetCanWind()
     {
         this.canWind = !this.canWind;
+        if (this.canWind)
+        {
+            buttonSprite.color = Color.green;
+        }
+        else
+        {
+            buttonSprite.color = Color.red;
+        }
     }
 
     private void OnMouseDown()
